2cd79aa613b1d030afc2ae55cbcd3c7248d5bc40
joão <joaovitormachadororato@gmail.com>
2023-10-25 19:43:53 -0300

    tests updated

--- src/database/knex/migrations/20231025223506_Favorites.js
+++
@@ -1,9 +0,0 @@
-exports.up = knex => knex.schema.createTable("favorites", table => {
-    table.integer("order_id");
-    table.integer("user_id").references("id").inTable("users");
-    table.integer("plate_id");
-
-    table.timestamp("created_at").defaultTo(knex.fn.now());
-});
-
-exports.down = knex => knex.schema.dropTable("favorites");
