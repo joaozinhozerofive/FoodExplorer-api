e9957e72802f9482840413b806b690cfc0df6f39
joão <joaovitormachadororato@gmail.com>
2023-11-07 22:05:41 -0300

    testes

--- src/Controllers/OrdersController.js
+++ src/Controllers/OrdersController.js
@@ -49,6 +49,7 @@ class OrdersController{
         //Aqui estou acessando todas as linhas da tabela orders que contenham o user_id enviado na requisição. 
         const orders = await knex("orders").where({user_id}).orderBy("order_id", "desc");
 
+
         //Aqui estou acessando todas as linhas da tabela carts, porém selecionando também name e price na tabela plates
         const ordersItems = await knex("carts")
       .select([
@@ -59,13 +60,15 @@ class OrdersController{
       ])
       .innerJoin("plates", "carts.plate_id", "plates.plate_id") //Aqui estou me conectando com a tabela plates e fazendo uma espécie de "where" --- .where("cars.plate_id", "plates.plate_id").
 
+      const newData = new Date()
+
       // aqui estou criando uma variavel que percorre cada objeto de orders
       const ordersWithItems = orders.map(order => { //o map está percorrendo cada objeto de orders e os nomeando como order 
         const orderItem = ordersItems.filter(item => item.order_id === order.order_id )// aqui a variavel orderItem esta percorrendo cada linha da tabela carts e as nomeando como item e retornando
         //na variavel ordersWithItems estou executando uma função que separa os objetos na tabela cart que correspondem a order_id da tabela orders que esta sendo mapeado no momento
         return {
             order, 
-            items: orderItem
+            items: orderItem, 
         }
       })
 
